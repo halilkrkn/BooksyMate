b69ac8820e258f8ab9c566aa3c75eeef20ee9fd3
halilkrkn <Halilkrkn000@gmail.com>
2024-12-21 02:10:56 +0300

    Done Setup Project Dependencies

---
+++ backend/BooksyMate/src/main/java/com/halilkrkn/BooksyMate/BooksyMateApiApplication.java
@@ -0,0 +1,13 @@
+package com.halilkrkn.BooksyMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksyMateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksyMateApiApplication.class, args);
+	}
+
+}
---
+++ backend/BooksyMate/src/test/java/com/halilkrkn/BooksyMate/BooksyMateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halilkrkn.BooksyMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksyMateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
