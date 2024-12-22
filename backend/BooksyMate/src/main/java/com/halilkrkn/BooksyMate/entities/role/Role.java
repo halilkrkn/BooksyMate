401aab78b82ce650a1bad0fe128efcaef7105e8d
halilkrkn <Halilkrkn000@gmail.com>
2024-12-22 20:41:48 +0300

    Created Role Entity for Users

---
+++ backend/BooksyMate/src/main/java/com/halilkrkn/BooksyMate/entities/role/Role.java
@@ -0,0 +1,42 @@
+package com.halilkrkn.BooksyMate.entities.role;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.halilkrkn.BooksyMate.entities.concretes.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "_role")
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> users;
+
+
+    @CreatedDate
+    @Column(name = "created_date", nullable = false, updatable = false)
+    private LocalDate createdDate;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDate lastModifiedDate;
+
+}
