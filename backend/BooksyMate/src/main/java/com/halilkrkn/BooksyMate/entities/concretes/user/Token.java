32097968687c09591e7fc15bae6b0466d2ea1976
halilkrkn <Halilkrkn000@gmail.com>
2024-12-22 21:01:32 +0300

    Created Token Entity and Token Repository

---
+++ backend/BooksyMate/src/main/java/com/halilkrkn/BooksyMate/entities/concretes/user/Token.java
@@ -0,0 +1,30 @@
+package com.halilkrkn.BooksyMate.entities.concretes.user;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+}
