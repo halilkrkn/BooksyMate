ec57d8ddb1676852517500eb0e2dcbf04a756301
halilkrkn <Halilkrkn000@gmail.com>
2025-01-04 00:37:53 +0300

    Has Been Included Open Api Documentation (OpenApiConfig)

---
+++ backend/BooksyMate/src/main/java/com/halilkrkn/BooksyMate/core/config/OpenApiConfig.java
@@ -0,0 +1,52 @@
+package com.halilkrkn.BooksyMate.core.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Halil İbrahim Karkın",
+                        email = "halilkrkn000@gmail.com"
+                ),
+                description = "Open API documentation for Spring Security Project",
+                title = "BooksyMate API",
+                version = "1.0.0",
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "http://www.apache.org/licenses/LICENSE-2.0"
+                ),
+                termsOfService = "http://swagger.io/terms/"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8088/api/v1"
+                ),
+                @Server(
+                        description = "PROD ENV",
+                        url = "https://booksymate.herokuapp.com/api/v1"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT Auth Token",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
