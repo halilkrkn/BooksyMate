2f8a2de75e6f69eaa991d5a740393b15b3f4006a
halilkrkn <Halilkrkn000@gmail.com>
2025-01-08 18:39:14 +0300

    Has Been Implemented the Book List Page

---
+++ frontend/booksy-mate/src/app/modules/book/pages/book-list/book-list.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit} from '@angular/core';
+import {BookService} from '../../../../services/services/book.service';
+import {Router} from '@angular/router';
+import {PageResponseBookResponse} from '../../../../services/models/page-response-book-response';
+import {NgForOf} from '@angular/common';
+
+@Component({
+  selector: 'app-book-list',
+  imports: [
+    NgForOf
+  ],
+  templateUrl: './book-list.component.html',
+  styleUrl: './book-list.component.scss'
+})
+export class BookListComponent implements OnInit {
+  protected bookResponse: PageResponseBookResponse = {};
+  private page: number = 0;
+  private size: number = 10;
+
+  constructor(
+    private router: Router,
+    private bookService: BookService,
+  ) { }
+
+  ngOnInit(): void {
+        this.findAllBooks();
+    }
+
+  private findAllBooks() {
+    this.bookService.findAllBooks({
+      page: this.page,
+        size: this.size,
+      }).subscribe({
+      next: (books: PageResponseBookResponse) => {
+        this.bookResponse = books;
+      }
+    });
+  }
+}
