05ce0df0b83d43eae9c47a4b84bd419e3d447f88
halilkrkn <Halilkrkn000@gmail.com>
2025-01-11 17:12:47 +0300

    Has Been Implement the Pagination for Book List

--- frontend/booksy-mate/src/app/modules/book/pages/book-list/book-list.component.ts
+++ frontend/booksy-mate/src/app/modules/book/pages/book-list/book-list.component.ts
@@ -2,27 +2,31 @@ import {Component, OnInit} from '@angular/core';
 import {BookService} from '../../../../services/services/book.service';
 import {Router} from '@angular/router';
 import {PageResponseBookResponse} from '../../../../services/models/page-response-book-response';
-import {NgForOf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 import {BookCardComponent} from '../../components/book-card/book-card.component';
 
 @Component({
   selector: 'app-book-list',
   imports: [
     NgForOf,
-    BookCardComponent
+    BookCardComponent,
+    NgIf
   ],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.scss'
 })
 export class BookListComponent implements OnInit {
+
   bookResponse: PageResponseBookResponse = {};
   page: number = 0;
-  size: number = 10;
+  size: number = 2;
+  pages: any = [];
 
   constructor(
     private router: Router,
     private bookService: BookService,
-  ) {}
+  ) {
+  }
 
   ngOnInit(): void {
     this.findAllBooks();
@@ -31,11 +35,46 @@ export class BookListComponent implements OnInit {
   private findAllBooks() {
     this.bookService.findAllBooks({
       page: this.page,
-      size: this.size,
-    }).subscribe({
-      next: (books: PageResponseBookResponse) => {
-        this.bookResponse = books;
-      }
-    });
+      size: this.size
+    })
+      .subscribe({
+        next: (books) => {
+          this.bookResponse = books;
+          this.pages = Array(this.bookResponse.totalPages)
+            .fill(0)
+            .map((x, i) => i);
+        }
+      });
   }
+
+  gotToPage(page: number) {
+    this.page = page;
+    this.findAllBooks();
+  }
+
+  goToFirstPage() {
+    this.page = 0;
+    this.findAllBooks();
+  }
+
+  goToPreviousPage() {
+    this.page--;
+    this.findAllBooks();
+  }
+
+  goToLastPage() {
+    this.page = this.bookResponse.totalPages as number - 1;
+    this.findAllBooks();
+  }
+
+  goToNextPage() {
+    this.page++;
+    this.findAllBooks();
+  }
+
+  get isLastPage() {
+    return this.page === this.bookResponse.totalPages as number - 1;
+  }
+
+
 }
