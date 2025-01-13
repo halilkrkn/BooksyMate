fdda4e85896d329c5301ddfdd6af43ecb4384116
halilkrkn <Halilkrkn000@gmail.com>
2025-01-13 18:00:36 +0300

    Has Been Implement the Update a Book

--- frontend/booksy-mate/src/app/modules/book/pages/manage-book/manage-book.component.ts
+++ frontend/booksy-mate/src/app/modules/book/pages/manage-book/manage-book.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgForOf, NgIf} from '@angular/common';
 import {BookRequest} from '../../../../services/models/book-request';
 import {FormsModule} from '@angular/forms';
-import {Router, RouterLink} from '@angular/router';
+import {ActivatedRoute, Router, RouterLink} from '@angular/router';
 import {BookService} from '../../../../services/services/book.service';
+import {BookResponse} from '../../../../services/models/book-response';
 
 @Component({
   selector: 'app-manage-book',
@@ -16,7 +17,7 @@ import {BookService} from '../../../../services/services/book.service';
   templateUrl: './manage-book.component.html',
   styleUrl: './manage-book.component.scss'
 })
-export class ManageBookComponent {
+export class ManageBookComponent implements OnInit {
 
   bookRequest: BookRequest = {authorName: '', isbn: '', synopsis: '', title: ''};
   errorMessage: Array<string> = [];
@@ -25,10 +26,37 @@ export class ManageBookComponent {
 
   constructor(
     private bookService: BookService,
-    private router: Router
+    private router: Router,
+    private activatedRoute: ActivatedRoute
   ) {}
 
-  onFileChange(event: any) {
+
+  // Buradaki olay edit tuşuna basıldığında kitabın bilgilerini getirip formda göstermek
+  ngOnInit(): void {
+    const bookId = this.activatedRoute.snapshot.params['bookId'];
+
+    if (bookId) {
+      this.bookService.findBookById({
+        "book-id": bookId
+      }).subscribe({
+        next: (book: BookResponse) => {
+          this.bookRequest = {
+            id: book.id,
+            title: book.title as string,
+            authorName: book.authorName as string,
+            isbn: book.isbn as string,
+            synopsis: book.synopsis as string,
+            shareable: book.shareable as boolean
+          }
+          if (book.cover) {
+            this.selectedPicture = 'data:image/jpg;base64,' + book.cover;
+          }
+        }
+      });
+    }
+  }
+
+  onFileSelected(event: any) {
     this.selectedBookCover = event.target.files[0];
     console.log(this.selectedBookCover);
 
